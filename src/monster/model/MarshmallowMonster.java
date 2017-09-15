cde86216c9c47ce26c2724b226f6af61f4278b20
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-15 12:19:56 -0600

    Created a string method called description that uses all the data member to describe a "monster" with fun text and the values of the various data members.
    Also fixed an issue with the initialization part being inside of the defualt constructor which is wrong. ALSO - the message from the last commit message also refers to this one instead of that one. I accidentally didn't save on the eclipse which didn't transfer it here correctly.

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,42 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	//Default constructor (void turns it into a method which you DON'T want here)
+	public MarshmallowMonster()
+	{
+		
+	}
+	
+	//initializes everything to 0, false, or null. 
+	public MarshmallowMonster(String name, int eyeCount, int arms, int tentacleCount, boolean hasBloop)	//names don't have to have a match. But it's a good idea to put them in order
+	{
+		this.name = name;	//Assigns to the class data member this.name the value of "name" (right to left/one way road)
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleCount;
+		this.hasBloop = hasBloop;
+			
+	}
+		
+	public String toString()
+	{	//example of how to use all the data members in the description of the monster
+//		String description = "I am a monster, my name is " + name + ", I have " + eyeCount + " eyes and only" ;	//command / comments and uncomment code
+//		description += tentacleAmount + " tentacles but I have " + armCount + " arms! " + hasBloop + " tells you"; //description += says continue onto the string and keep adding
+//		description += " if I have a bloop";
+			
+		String description = "I am a horifying monster to behold named " + name + " with " + eyeCount + " eyes to stare into your soul.";
+		description += " My " + tentacleAmount + " tentacle's will snatch you up if you get too close and my " + armCount + " arms will";
+		description += " let me skitter closer and closer! And i will hunt you down with my " + hasBloop + " bloop. Hey! it's a lot more intimidating than it sounds! ";
+		
+		return description;
+	}
+	
 }
