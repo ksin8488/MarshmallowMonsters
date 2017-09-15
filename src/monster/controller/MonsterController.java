0b51c35041e891d88c29341107496d30d6bf5fa7
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-15 13:20:51 -0600

    Imported the scanner into the program and used it in a new method called interactWithMonster in the controller. Also added the setup in MarsmallowMonster for giving the monster a backstroy when filled in with text.
    the interactWithMonster will allow the user to enter how many eyes they will want to eat then subtracts it from getEyeCount and then displays the text with the new altered values for the eyes.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,6 +1,7 @@
 package monster.controller;
 
 import monster.model.MarshmallowMonster;
+import java.util.Scanner;
 
 public class MonsterController
 {
@@ -13,6 +14,19 @@ public class MonsterController
 		System.out.println("I am feeling hungery, I am going to eat one of Kothan's arms" );
 		kothan.setArmCount(kothan.getArmCount() - 1);
 		System.out.println(kothan);
+		
+		interactWithMonster(kothan);
 				
+	}
+	
+	private void interactWithMonster(MarshmallowMonster currentMonster)
+	{
+		Scanner myScanner= new Scanner(System.in);
+		System.out.println(currentMonster.getName() + " wants to know how many eyes you want to eat. Please type in how many you want to eat. ");
+		int consumed = myScanner.nextInt();
+		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
+		System.out.println(currentMonster);
+		
+		myScanner.close(); //closes scanner to prevent memory leaks
 	}
 }
