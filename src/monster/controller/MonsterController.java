3a15b542d77fa673f5406fae182ccfd60376e81f
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-19 12:41:06 -0600

    Added an if/else if/else statements to create a filter for inputs that user enters for eating arms.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -27,6 +27,28 @@ public class MonsterController
 		currentMonster.setEyeCount(currentMonster.getEyeCount() - consumed);
 		System.out.println(currentMonster);
 		
+		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
+		//consumed = myScanner.nextInt();		can do it this way
+		int armEat = myScanner.nextInt();		//or use a new value if you want to record the value for another time else just reuse
+		
+		if(armEat == 0)			//== operator can only compare ints or booleans in Java
+		{
+			System.out.println("Not hungry? oh so sad...");
+		}
+		else if(armEat < 0)
+		{
+			System.out.println("Math/Reality is hard for you. It's impossible to eat a negative amount.");
+		}
+		else if(armEat - currentMonster.getArmCount() > 0)
+		{
+			System.out.println("You are not allowed to eat more arms than exist on me :( ");
+		}
+		else 
+		{
+			currentMonster.setArmCount(currentMonster.getArmCount() - armEat);
+			System.out.println("OK, now I have " + currentMonster.getArmCount());
+		}
+		
 		myScanner.close(); //closes scanner to prevent memory leaks
 	}
 }
