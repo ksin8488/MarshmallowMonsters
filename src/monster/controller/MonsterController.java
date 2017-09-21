1f8b6abd166b578fa93d59b63348daf7625c4f1c
ksin8488 <31518706+ksin8488@users.noreply.github.com>
2017-09-21 13:31:38 -0600

    Finished converting the rest of the program from console display to popup displays. Edited the code so it would use helper methods to scan user inputs to prevent crashes. Did some editing and notes to clean up and fix errors or bugs.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -50,9 +50,17 @@ public class MonsterController
 //		System.out.println(currentMonster);
 		popup.displayText(currentMonster.toString());
 		
-		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
+//		System.out.println("How many arms are you interested in eating? I have " + currentMonster.getArmCount());
+		String armResponse = popup.getResponse("How many arms are you interested in eating? He has " + currentMonster.getArmCount());
+//		response = popup.getResponse("");		//catches the response so it doesn't disappear
 		//consumed = myScanner.nextInt();		can do it this way
-		int armEat = myScanner.nextInt();		//or use a new value if you want to record the value for another time else just reuse
+//		int armEat = myScanner.nextInt();		//or use a new value if you want to record the value for another time else just reuse
+		int armEat = 0;
+		
+		if(isValidInteger(armResponse))
+		{
+			armEat = Integer.parseInt(armResponse);
+		}
 		
 		if(armEat == 0)			//== operator can only compare ints or booleans in Java
 		{
@@ -77,11 +85,10 @@ public class MonsterController
 		}
 		
 //		System.out.println("How many tentacles do you want to eat? I have " + currentMonster.getTentacleAmount());
-		popup.getResponse("How many tentacles do you want to eat? I have " + currentMonster.getTentacleAmount());
+		String tentacleResponse = popup.getResponse("How many tentacles do you want to eat? I have " + currentMonster.getTentacleAmount());
 		
 		
 		double food = 0.0;
-		String tentacleResponse = popup.getResponse("");
 		if(isValidDouble(tentacleResponse))
 		{
 			food = Double.parseDouble(tentacleResponse);
@@ -99,60 +106,89 @@ public class MonsterController
 		}
 		
 		//Uses many if/else if/else statements to make sure that 
-		System.out.println("what color would you like to paint the whtie monster? 1 for red, 2 Green, 3 for Blue, 4 White, 5 Black ");
-		int userColor = myScanner.nextInt();
+//		System.out.println("what color would you like to paint the white monster? 1 for red, 2 Green, 3 for Blue, 4 White, 5 Black ");
+		String colorResponse = popup.getResponse("What color would you like to paint the white monster? 1 for red, 2 Green, 3 for Blue, 4 White, 5 Black ");
+		int userColor = 0;
+		 
+		if(isValidInteger(colorResponse))
+		{
+			userColor = Integer.parseInt(colorResponse);
+		}
 		
 		if(userColor == 1 )
 		{
-			System.out.println("You throw red sugary color on it. It looks bright red! But then the body absorbs the sugar... ");
+//			System.out.println("You throw red sugary color on it. It looks bright red! But then the body absorbs the sugar... ");
+			popup.displayText("You throw red sugary color on it. It looks bright red! But then the body absorbs the sugar... ");
 		}
 		else if(userColor == 2 )
 		{
 			System.out.println("You throw green sugary color on it. It looks bright green! But then the body absorbs the sugar... ");
+			popup.displayText("You throw green sugary color on it. It looks bright green! But then the body absorbs the sugar... ");
 		}
 		else if(userColor == 3 )
 		{
-			System.out.println("You throw blue sugary color on it. It looks bright blue! But then the body absorbs the sugar... ");
+//			System.out.println("You throw blue sugary color on it. It looks bright blue! But then the body absorbs the sugar... ");
+			popup.displayText("You throw blue sugary color on it. It looks bright blue! But then the body absorbs the sugar... ");
 		}
 		else if(userColor == 4 )
 		{
-			System.out.println("You throw white sugary color on it. It's already white but it seems to appreciate it... before eating it. ");
+//			System.out.println("You throw white sugary color on it. It's already white but it seems to appreciate it... before eating it. ");
+			popup.displayText("You throw white sugary color on it. It's already white but it seems to appreciate it... before eating it. ");
 		}
 		else if(userColor == 5 )
 		{
-			System.out.println("You throw black sugary color on it. It rises and puffs up trying to look scary. But he just looks sweet as the sugar is absorbed into him... ");
-		}
-		//more else if/else statements to prevent errors
-		if(userColor != 1)
-		{
-			System.out.println("You did not choose a color number");
-		}
-		else if(userColor != 2)
-		{
-			System.out.println("You did not choose a color number");
+//			System.out.println("You throw black sugary color on it. It rises and puffs up trying to look scary. But he just looks sweet as the sugar is absorbed into him... ");
+			popup.displayText("You throw black sugary color on it. It rises and puffs up trying to look scary. But he just looks sweet as the sugar is absorbed into him... ");
 		}
 		
-		else if(userColor != 3)
-		{
-			System.out.println("You did not choose a color number");
-		}
-		else if(userColor != 4)
-		{
-			System.out.println("You did not choose a color number");
-		}
-		else if(userColor != 5)
-		{
-			System.out.println("You did not choose a color number");
-		}
-		else
-		{
-			System.out.println("ERROR! Something has gone wrong with your selection. ");
-		}
+		//DON'T NEED DUE TO HELPER METHODS
+//		//more else if/else statements to prevent errors
+//		if(userColor != 1)
+//		{
+////			System.out.println("You did not choose a color number");
+//			popup.displayText("You did not choose a color number");
+//		}
+//		else if(userColor != 2)
+//		{
+////			System.out.println("You did not choose a color number");
+//			popup.displayText("You did not choose a color number");
+//		}
+//		
+//		else if(userColor != 3)
+//		{
+////			System.out.println("You did not choose a color number");
+//			popup.displayText("You did not choose a color number");
+//		}
+//		else if(userColor != 4)
+//		{
+////			System.out.println("You did not choose a color number");
+//			popup.displayText("You did not choose a color number");
+//		}
+//		else if(userColor != 5)
+//		{
+////			System.out.println("You did not choose a color number");
+//			popup.displayText("You did not choose a color number");
+//		}
+//		else
+//		{
+////			System.out.println("ERROR! Something has gone wrong with your selection. ");
+//			popup.displayText("ERROR! Something has gone wrong with your selection");
+//		}
 		
 		popup.displayText("Hi there! Ready to play???");
-		String response2 = popup.getResponse("What is the air speed of a coconut laden swallow?");
-		System.out.println(response2);
+		String questionResponse = popup.getResponse("What is the air speed of a coconut laden swallow that isn't coconut laden?");
+//		System.out.println(response2);
+		boolean correctAnswer = false;
 		
+		if (questionResponse.equals("24"))
+		{
+			correctAnswer = true;
+		}
+		
+		if(isValidBoolean(questionResponse))
+		{
+			popup.displayText("You got the answer! Congratz! Cheater.");
+		}
 		
 		myScanner.close(); //closes scanner to prevent memory leaks
 	}
@@ -175,7 +211,7 @@ public class MonsterController
 		return valid;
 	}
 	
-	private boolean isValidDouble(String sampleDouble)
+	private boolean isValidDouble(String sampleDouble)	//checks to see if user's entry is a valid double
 	{
 		boolean valid = false;
 		
@@ -189,6 +225,22 @@ public class MonsterController
 			popup.displayText("You need to typ a double - " + sampleDouble + "is not a valid answer.");
 		}
 		
+		return valid;
+	}
+	
+	private boolean isValidBoolean(String sampleBoolean)	//checks to see if user's entry is a valid boolean
+	{
+		boolean valid = false;
+		
+		try
+		{
+			Boolean.parseBoolean(sampleBoolean);
+			valid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			popup.displayText("You need to type a boole- " + sampleBoolean + "is not a valid answer.");
+		}
 		return valid;
 	}
 }
